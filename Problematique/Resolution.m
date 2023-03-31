61f4598112dd9144548a406721dfd8c3e854a049
Daniel bilodeau <danyboy-2000@live.com>
2023-03-31 16:35:51 -0400

    Trapèze-simpson et identification, bug Pdyn ou normal?

--- Problematique/Resolution.m
+++ Problematique/Resolution.m
@@ -1,3 +1,93 @@
 clc
 clear all
 close all
+
+format short G
+load('accelero_Data_from_NASA')
+constantes
+
+acc_mes = -acc_mes;
+
+% Méthode des trapèzes pour faire l'intégrale de l'accélération
+N = length(t);
+h = t(2) - t(1);
+v_mes(1,1) = 0 + v_ini;
+for i = 2:N
+    v_mes(i,1) = v_ini + (acc_mes(1) + acc_mes(i) + 2*sum(acc_mes(2:i-1))) * h/2;
+end
+% Erreur de la méthode des trapèzes
+df_a = (acc_mes(2) - acc_mes(1))/h;
+df_b = (acc_mes(N-1) - acc_mes(N))/h;
+Err_vit = h^2/12*(df_b - df_a);
+
+%méthode de simpson pour faire l'intégrale de la vitesse
+h_mes(1,1) = h_ini;
+xs(1) = t(1);
+for i = 3:2:N
+    h_mes((i-1)/2 + 1,1) = h_ini - (v_mes(1) + v_mes(i) + 4*sum(v_mes((2:2:i-1))) + 2*sum(v_mes(3:2:i-1))) * h/3;
+    xs((i-1)/2 + 1) = t(i);
+end
+% Erreur de la méthode des trapèzes
+d3f_a = (v_mes(4) - 3*v_mes(3) + 3*v_mes(2) - v_mes(1))/h^3;
+d3f_b = (v_mes(N) - 3*v_mes(N-1) + 3*v_mes(N-2) - v_mes(N-3))/h^3;
+Err_pos = h^4/180*(d3f_b - d3f_a);
+
+% Identification des paramètres (boite grise) en trouvant la forme linéaire
+gr_sin_gamma = -MUmars./(Rmars + h_mes).^2;
+Daero = -m.*(acc_mes(1:2:end) + gr_sin_gamma);
+Pdyn = Daero/(S * CD0);
+
+X = h_mes;
+Y = log((2.*Pdyn)./(v_mes(1:2:end).^2));
+
+mat1 = [N       sum(X);
+        sum(X)  sum(X.^2)];
+
+mat2 = [sum(Y);
+        sum(Y.*X)];
+
+params = inv(mat1) * mat2;
+hs = -1/params(2);
+rho0 = exp(params(1));
+
+P_obt = 0.5*(rho0*exp(-h_mes./hs)).*v_mes(1:2:end).^2;
+% code pour calcul de l'erreur copié du laboratoire
+% E = sum((P_obt - P).^2);
+% RMS = sqrt(1/N *E);
+% moy = sum(P)/N;
+% R_2 = sum((P_obt - moy).^2)/sum((P - moy).^2);
+
+
+
+
+%% figures
+figure()
+plot(t, acc_mes, 'o')
+
+grid minor
+xlabel('temps (s)')
+ylabel('Accélération (m/s^2)')
+legend('acc mesuré')
+
+figure()
+plot(t, v_mes, 'o')
+
+grid minor
+xlabel('temps (s)')
+ylabel('Vitesse (m/s)')
+legend('vit mesuré')
+
+figure()
+plot(xs, h_mes, 'o')
+
+grid minor
+xlabel('temps (s)')
+ylabel('altitude (m)')
+legend('altitude mesuré')
+
+figure()
+plot(t(1:2:end), Pdyn)
+hold on
+plot(t(1:2:end), P_obt)
+grid minor
+legend('Pdyn calc. avec Daero', 'Pdyn calc avec hs et rho0')
---
+++ Problematique/constantes.m
@@ -0,0 +1,27 @@
+m = 50; %50 kg
+J = 1.5; %kg/m^2
+Rmars = 3397e03; %m
+MUmars = 42830e09; %m^3/s^2
+S = 0.8; %m^2
+d = 0.05; % m
+CD0 = 1.2;
+CLalpha = 0.8;
+CMalpha = -0.07;
+CMq = -0.05;
+CMdelta = 0.1;
+
+v_ini = 6100; %m/s
+gamma_ini = deg2rad(-20.5);%rad
+h_ini = 120000; %m
+s_ini = deg2rad(0);%rad
+teta_ini = deg2rad(-80); %rad
+q_ini = deg2rad(0); %rad/s
+
+%conditions finales désirés
+vfin = 250; %ou 300 m/s
+hfin = 10000; % m
+
+%Contraintes
+dt_lim = 40; %s
+Pdyn_max = 9500; %N/m^2
+teta_cmd = 60; %degrés
