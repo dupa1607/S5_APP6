9ff047b6636916f97244ccbebf5845762f224e10
Daniel bilodeau <danyboy-2000@live.com>
2023-03-31 21:39:00 -0400

    debugging, les graphs obtenus semblent être shifté d'une valeur??

--- Problematique/Resolution.m
+++ Problematique/Resolution.m
@@ -18,30 +18,32 @@ end
 % Erreur de la méthode des trapèzes
 df_a = (acc_mes(2) - acc_mes(1))/h;
 df_b = (acc_mes(N-1) - acc_mes(N))/h;
-Err_vit = h^2/12*(df_b - df_a);
+Err_vit = (h^2/12)*(df_b - df_a);
 
 %méthode de simpson pour faire l'intégrale de la vitesse
 h_mes(1,1) = h_ini;
-xs(1) = t(1);
-for i = 3:2:N
-    h_mes((i-1)/2 + 1,1) = h_ini + ((v_mes(1) + v_mes(i) + 4*sum(v_mes((2:2:i-1))) + 2*sum(v_mes(3:2:i-1))) * h/3);
-    xs((i-1)/2 + 1) = t(i);
+ts(1) = t(1);
+Nm = N + 1;
+for i = 3:2:Nm
+    h_mes((i-1)/2 + 1,1) = h_ini - ((v_mes(1) + v_mes(i) + 4*sum(v_mes((2:2:i-1))) + 2*sum(v_mes(3:2:i-1))) * h/3);
+    ts((i-1)/2 + 1) = t(i);
 end
-% Erreur de la méthode des trapèzes
+ts = ts';
+% Erreur de la méthode simpson
 d3f_a = (v_mes(4) - 3*v_mes(3) + 3*v_mes(2) - v_mes(1))/h^3;
 d3f_b = (v_mes(N) - 3*v_mes(N-1) + 3*v_mes(N-2) - v_mes(N-3))/h^3;
 Err_pos = h^4/180*(d3f_b - d3f_a);
 
 % Identification des paramètres (boite grise) en trouvant la forme linéaire
-gr_sin_gamma = -MUmars./(Rmars + h_mes).^2;
+gr_sin_gamma = -MUmars./((Rmars + h_mes).^2);
 Daero = -m.*(acc_mes(1:2:end) + gr_sin_gamma);
-Pdyn = Daero/(S * CD0);
+Pdyn = Daero./(S * CD0);
 
 X = h_mes;
 Y = log((2.*Pdyn)./(v_mes(1:2:end).^2));
 
-mat1 = [length(h_mes)       sum(X);
-        sum(X)  sum(X.^2)];
+mat1 = [length(ts)  sum(X);
+        sum(X)      sum(X.^2)];
 
 mat2 = [sum(Y);
         sum(Y.*X)];
@@ -50,7 +52,11 @@ params = inv(mat1) * mat2;
 hs = -1/params(2);
 rho0 = exp(params(1));
 
-P_obt = 0.5*(rho0*exp(-h_mes./hs)).*v_mes(1:2:end).^2;
+P_obt = 0.5*(rho0*exp(-h_mes./hs)).*(v_mes(1:2:end).^2);
+Daero_obt = P_obt * S *CD0;
+acc_approx = -Daero_obt/m - gr_sin_gamma;
+%Les données semblent être shiftés d'une case ???
+% acc_approx = circshift(acc_approx,1)
 
 % code pour calcul de l'erreur copié du laboratoire
 E = sum((P_obt - Pdyn).^2);
@@ -63,33 +69,41 @@ R_2 = sum((P_obt - moy).^2)/sum((Pdyn - moy).^2);
 
 
 %% figures
+% ts = ts + 1 % Il semble y avoir une erreur de shift de 1 dans les données
 figure()
 plot(t, acc_mes, 'o')
+hold on
+plot(ts, acc_approx)
 
 grid minor
 xlabel('temps (s)')
 ylabel('Accélération (m/s^2)')
-legend('acc mesuré')
+legend('acc mesuré', 'acc obtenu')
+title('Accélératione du test NASA en fonction du temps')
 
-figure()
-plot(t, v_mes, 'o')
 
-grid minor
-xlabel('temps (s)')
-ylabel('Vitesse (m/s)')
-legend('vit mesuré')
-
-figure()
-plot(xs, h_mes, 'o')
-
-grid minor
-xlabel('temps (s)')
-ylabel('altitude (m)')
-legend('altitude mesuré')
-
-figure()
-plot(t(1:2:end), Pdyn, 'o')
-hold on
-plot(t(1:2:end), P_obt)
-grid minor
-legend('Pdyn calc. avec Daero', 'Pdyn calc avec hs et rho0')
+% figure()
+% plot(t, v_mes, 'o')
+% 
+% grid minor
+% xlabel('temps (s)')
+% ylabel('Vitesse (m/s)')
+% legend('vit mesuré')
+% 
+% figure()
+% plot(ts, h_mes, 'o')
+% 
+% grid minor
+% xlabel('temps (s)')
+% ylabel('altitude (m)')
+% legend('altitude mesuré')
+% 
+% %Les données semblent être shiftés d'une case ???
+% % P_obt = circshift( P_obt , 1 )
+% figure()
+% plot(ts, Pdyn, 'o')
+% hold on
+% plot(ts, P_obt)
+% grid minor
+% legend('Pdyn calc. avec Daero', 'Pdyn calc avec hs et rho0')
+% title('Pression dynamique comparaison')
